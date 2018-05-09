b6e6efc8c9a6130bccc8d6d14eb2e902222aed3c
lius25 <julius.sisfo@student.ac.id>
2018-05-09 16:13:10 +0700

    perbaikan lagi

---
+++ model/Mahasiswa.php
@@ -0,0 +1,17 @@
+<?php 
+
+namespace Model;
+
+ class Mahasiswa extends Model{
+ 	public $nim;
+ 	public $ipk;
+
+ 	public function munculkannama()
+
+ 	{
+ 		echo "hallo saya sisfo";
+ 	}
+ }
+
+
+ ?>
---
+++ model/Model.php
@@ -0,0 +1,8 @@
+<?php 
+namespace MOdel;
+class Model
+{
+	public $config;
+}
+	
+ ?>
